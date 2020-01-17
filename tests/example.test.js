04eff98ea3b1405e73c18a74eb09d1957eb7783d
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-17 13:38:12 -0300

    framework part 4 feedback page

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,17 +1,20 @@
 import HomePage from '../pages/HomePage';
 import TopBar from '../pages/components/TopBar';
 import LoginPage from '../pages/LoginPage';
+import FeedbackPage from '../pages/FeedbackPage';
 
 describe('Example', () => {
     let homepage;
     let topbar;
     let loginpage;
+    let feedbackpage;
 
     beforeAll(async () =>{
         jest.setTimeout(15000);
         homepage = new HomePage();
         topbar = new TopBar();
         loginpage = new LoginPage();
+        feedbackpage = new FeedbackPage();
     })
 
     it('home page should work', async () => {
@@ -21,13 +24,18 @@ describe('Example', () => {
         await homepage.isNavbarDisplayed();
         await topbar.isTopBarDisplayed()
      });
+     /*
      it('try to login', async () => {
         await loginpage.visit();
         await loginpage.isLoginFormDisplayed();
         await loginpage.login('username','password');
         await loginpage.wait(5000);
-       
+       });*/
+       it('feedback should work', async () => {
+         await feedbackpage.visit();
+         await feedbackpage.isFeedbackFormDisplayed();
+         await feedbackpage.submitFeedback('Igor','email@email.com','subject','comment');
+         await feedbackpage.wait(5000);
      });
-
 
 })
