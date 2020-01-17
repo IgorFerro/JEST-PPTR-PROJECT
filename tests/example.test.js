9f9c059b48f672a638434ded33d7fdfbcd1f045b
Igor Ferro Parreira <ifparreira@uolinc.com>
2020-01-17 13:19:47 -0300

    Framework part 3 Login Page

--- tests/example.test.js
+++ tests/example.test.js
@@ -1,14 +1,17 @@
 import HomePage from '../pages/HomePage';
 import TopBar from '../pages/components/TopBar';
+import LoginPage from '../pages/LoginPage';
 
 describe('Example', () => {
     let homepage;
-    let topbar
+    let topbar;
+    let loginpage;
 
     beforeAll(async () =>{
         jest.setTimeout(15000);
         homepage = new HomePage();
         topbar = new TopBar();
+        loginpage = new LoginPage();
     })
 
     it('home page should work', async () => {
@@ -18,5 +21,13 @@ describe('Example', () => {
         await homepage.isNavbarDisplayed();
         await topbar.isTopBarDisplayed()
      });
+     it('try to login', async () => {
+        await loginpage.visit();
+        await loginpage.isLoginFormDisplayed();
+        await loginpage.login('username','password');
+        await loginpage.wait(5000);
+       
+     });
+
 
 })
